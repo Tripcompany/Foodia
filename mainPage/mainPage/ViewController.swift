0338ba3a0b813b5511301e9111a548921e77cc6d
minguking <58521437+minguking@users.noreply.github.com>
2020-03-08 16:59:50 +0900

    Add files via upload

---
+++ mainPage/mainPage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mainPage
+//
+//  Created by Kang Mingu on 08/03/2020.
+//  Copyright © 2020 Kang Mingu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func sb1ButtonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goTo1", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
